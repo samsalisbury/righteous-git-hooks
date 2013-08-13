66450d9324f4f5b3b796bcaf26c2c8772130ca32
Sam Salisbury <samsalisbury@gmail.com>
2013-08-13 22:34:18 +0100

    Improved test structure a bit.

--- test/when-solution-has-no-problems-exit-code-zero.rb
+++ test/when-solution-has-no-problems-exit-code-zero.rb
@@ -1,11 +1,46 @@
+# 1. Copy the solution and make a new git repo
+# 2. Init a new git repo, and commit status quo
+# 3. Get the commit sha, then poke around in the repo and try to commit, running the tests...
 
+require 'securerandom'
 
-result = `ruby hooks/go_go_righteous_git_hooks.rb $PWD test/GitHooksTestSolution/GitHooksTest GitHooksTest.csproj`
-
-if result.split("\n").last == 'Congratulations, this is a righteous commit!' then
-	puts 'Tests passed!'
-	exit 0
+def generate_dirname()
+	"~/.righteous-git-hooks-temp-test-repo-" + SecureRandom.uuid[0..7]
 end
 
-puts 'Tests failed!'
-exit 1
+temp_dir = File.expand_path(generate_dirname())
+if File.directory? temp_dir
+	puts "Directory #{temp_dir} already exists, can't continue."
+	exit 1
+end
+
+`mkdir #{temp_dir}`
+
+tests_root = File.expand_path(File.dirname(__FILE__))
+
+master_data_dir = File.join(tests_root, 'GitHooksTestSolution')
+hook_script = File.join(tests_root, "../hooks/go_go_righteous_git_hooks.rb")
+
+a_test_failed = 0
+
+Dir.chdir(File.expand_path(temp_dir)) do
+	# Arrange - copy files, create test repo and commit
+	`cp -r #{master_data_dir}/* #{temp_dir}`
+	`git init`
+	`git add -A`
+	`git commit -m 'Test commit.'`
+	# Act
+	result = `ruby #{hook_script} $PWD GitHooksTest GitHooksTest.csproj`
+	# Assert
+	if result.split("\n").last == 'Congratulations, this is a righteous commit!' then
+		puts 'Test passed!'
+	else
+		puts 'Test failed.'
+		a_test_failed += 1
+	end
+end
+
+# clean up directory
+`rm -rf #{temp_dir}`
+
+exit a_test_failed
