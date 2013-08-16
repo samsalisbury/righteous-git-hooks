16bcd95f950dcb2a5b01cb0daf4a67dcd38d79f7
Sam Salisbury <samsalisbury@gmail.com>
2013-08-16 23:13:14 +0100

    Headway with simple test harness

--- test/command-unit.rb
+++ test/command-unit.rb
@@ -1,35 +1,112 @@
 module CommandUnit
 
-	class TestFixture
+  @@scenarios = []
+  @@current_scenario = nil
+  attr_reader :current_scenario
 
-		def run
-			self.public_methods(false).each do |m|
-        next if m == __method__
-        puts "Name: #{m}"
-        puts self.send m rescue 'could not invoke'
+  def scenario(description, &block)
+    @@scenarios.push Scenario.new(description, &block)
+  end
+
+  def run
+    @@scenarios.each do |scenario|
+      scenario.run
+    end
+  end
+
+  def set_up(&set_up_block)
+    raise 'set_up must be called from inside a scenario block' if @@current_scenario == nil
+    @@current_scenario.set_up_block = set_up_block
+  end
+
+  def when_i(desc, &when_i_block)
+    raise 'when_i must be called from inside a scenario block' if @@current_scenario == nil
+    @@current_scenario.add_test Test.new(desc, &when_i_block)
+  end
+
+  def i_expect(desc, &i_expect_block)
+    raise 'i_expect must be called from inside a scenario block' if @@current_scenario == nil
+    @@current_scenario.current_test.add_expectation Expectation.new(desc, &i_expect_block)
+  end
+
+  class Scenario
+    def initialize(desc, &block)
+      @desc = desc
+      @block = block
+      @set_up_block = nil
+      @tests = []
+      @current_test = nil
+      @tear_down_block = nil
+    end
+
+    def run
+      puts "Running scenario: #{@desc}"
+      @@current_scenario = self
+      @block.call
+      @tests.each do |test|
+        puts "When I #{test.when_i_text}"
+        context = {}
+        test.set_up.call(context) unless test.set_up.nil?
+        test.when_i_block.call(context) unless test.when_i_block.nil?
+        puts 'I expect that:'
+        test.expectations.each do |expectation|
+          puts expectation.desc
+          expectation.block.call(context) unless expectation.block.nil?
+        end
       end
+      @@current_scenario = nil
     end
 
-    def bananarama
-      hands = ''
-      5.times { |bananas| hands += "#{bananas+1} bananas " }
-      hands
+    def add_test(test)
+      @tests.push test
+      @current_test = test
     end
 
-    def cheezy_dreams
-      cheeses = ''
-      10.times { |i| cheeses += "cheese #{fac(i+1)} " }
-      cheeses
+    attr_accessor :desc, :block, :set_up_block, :tests, :tear_down_block, :current_test
+  end
+
+  class Test
+    def initialize(when_i_text, &when_i_block)
+      @when_i_text = when_i_text
+      @when_i_block = when_i_block
+      @expectations = []
     end
-
-    def fac(x)
-      return 1 if x == 1
-      return x * fac(x-1)
+    attr_reader :when_i_text, :when_i_block, :expectations
+    def add_expectation(expectation)
+      @expectations.push expectation
     end
+  end
 
-	end
-
-  tests = TestFixture.new
-  tests.run
+  class Expectation
+    def initialize(expectation_text, &expectaton_block)
+      @desc = expectation_text
+      @block = expectaton_block
+    end
+    attr_accessor :desc, :block
+  end
 
 end
+
+include CommandUnit
+
+scenario 'When blahblahs are whatnots' do
+
+  set_up do |context|
+    puts 'set_up called!'
+    context[:thing] = 'some property set in set_up'
+  end
+
+  when_i "don't do anything" do |context|
+    # Doing nothing
+    puts 'when_i called!'
+    context[:thing2] = 'thing'
+  end
+
+  i_expect 'to see a nice success message' do |context|
+    puts 'i_expect called!'
+    context[:thing3] = 'thang'
+  end
+
+end
+
+run
